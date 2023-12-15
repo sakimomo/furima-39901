5736735be839695140d4241990ee9187f3cdd36c
sakimomo <otegamisaki1@icloud.com>
2023-12-15 15:24:21 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-# class Category < ActiveHash::Base
+ class Category < ActiveHash::Base
 #  self.data = [
 #    { id: 1, name: '---' },
 #    { id: 2, name: 'レディース' },
@@ -15,4 +15,4 @@
 #
 #  include ActiveHash::Associations
 #  has_many :items
-# end
+ end
