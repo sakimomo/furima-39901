798bbc4ed71c8d64d320d87214a4f999d3892210
sakimomo <otegamisaki1@icloud.com>
2023-12-23 20:03:42 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,6 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      puts @item.errors.full_messages
       render :new, status: :unprocessable_entity
     end
   end
