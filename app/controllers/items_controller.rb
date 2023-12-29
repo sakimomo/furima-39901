c8d93dae4b54e17bf4969795566d4b18326551e1
sakimomo <otegamisaki1@icloud.com>
2023-12-29 20:32:28 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy, :update]
   before_action :move_to_index, only: [:edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -12,15 +13,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
 
     if @item.update(item_params)
       redirect_to action: :show
@@ -43,9 +41,11 @@ class ItemsController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
   def move_to_index
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id != @item.user_id
       redirect_to action: :index
     elsif !user_signed_in?
@@ -57,4 +57,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :content, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :shipping_day_id, :price).merge(user_id: current_user.id)
   end
+
 end
